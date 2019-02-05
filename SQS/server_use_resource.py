abf1dd32ef9ba3d7964484a3a66ebd6d766c102c
rocky1991 <hongchuan91@gmail.com>
2019-02-05 16:42:24 -0700

    tested long polling and visibility timeout setting

--- SQS/server_use_resource.py
+++ SQS/server_use_resource.py
@@ -29,7 +29,43 @@ def check_queue():
 	# return request_flag,response_flag
 
 
+def test():
+	sqs = boto3.resource('sqs')
+	queue = sqs.create_queue(QueueName='test',Attributes={'ReceiveMessageWaitTimeSeconds':'20'})
+	queue.send_message(MessageBody='world')
+	msg = queue.receive_messages()[0]
+	print(msg.body)
 
+	queue.receive_messages()[0].delete()
+	msg = queue.receive_messages()[0]
+	time.sleep(1)
+	queue.send_message(MessageBody='anything')
+	print(msg.body)
+
+def test1():
+	sqs=boto3.resource('sqs')
+	queue= sqs.get_queue_by_name(QueueName='test')
+	queue.send_message(MessageBody='wrld')
+	queue.send_message(MessageBody='sdf')
+	msg = queue.receive_messages(VisibilityTimeout=5)
+	print(msg[0].body)
+	print(len(msg))
+# test1()
+def del_msg():
+	sqs=boto3.resource('sqs')
+	queue = sqs.get_queue_by_name(QueueName='test')
+	for msg in queue.receive_messages():
+		msg.delete()
+
+def illustrate_Visibility():
+	sqs=boto3.resource('sqs')
+	queue= sqs.get_queue_by_name(QueueName='test')
+	queue.send_message(MessageBody='hello')
+	queue.receive_messages(VisibilityTimeout=5)[0].body
+	queue.receive_messages(VisibilityTimeout=0)[0].body
+
+	queue.receive_messages()[0].delete()
+	queue.receive_messages(VisibilityTimeout=0,WaitTimeseconds=5)[0].body
 
 
 # def main():
