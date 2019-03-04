7c3bebfc5cced4645c841342b51d05a8eebe3334
rocky1991 <hongchuan91@gmail.com>
2019-03-04 12:29:57 -0700

    change scc name to scc-trial.py

--- SQS/server_use_clientAPI.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-def test_client_api():
-	sqs=boto3.client('sqs')
-	# sqs.create_queue(QueueName='test',Attributes={'ReceiveMessageWaitTimeSeconds':'5'})
-	queue = sqs.get_queue_url(QueueName='test')['QueueUrl']
-	print(queue)
-	print(sqs.get_queue_attributes(QueueUrl=queue))
-test()
