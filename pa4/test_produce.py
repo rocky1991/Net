ae616499b013dc66d000298ab25135d8bcc2a662
rocky1991 <hongchuan91@gmail.com>
2019-04-07 16:30:59 -0600

    add q1 logs, clean up log for q3 5 and 7

--- pa4/test_produce.py
+++ pa4/test_produce.py
@@ -5,6 +5,6 @@ def acked(err, msg):
               .format(msg.value(), err.str()))
     else:
         print("Message produced: {0}".format(msg.value()))
-p = Producer({'bootstrap.servers':'54.201.231.207:9092'})
-p.produce("mytopic",value="hello",callback=acked)
+p = Producer({'bootstrap.servers':'35.165.72.110:9092'})
+p.produce("topic1",value="hello",callback=acked)
 p.flush()
