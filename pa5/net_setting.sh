a38861df0f181bab9632ab5440986d7b79ba2d0a
rocky1991 <hongchuan91@gmail.com>
2019-04-14 21:29:05 -0600

    minor tc rule changes

--- pa5/net_setting.sh
+++ pa5/net_setting.sh
@@ -9,4 +9,4 @@ echo "Deleting current net rule.."
 sudo tc qdisc del dev eth0 root
 echo "Setting up net rule.."
 sudo tc qdisc add dev eth0 root handle 1: netem delay ${lat}ms loss ${loss}%
-sudo tc qdisc add dev eth0 parent 1:1 handle 10: tbf rate ${bw}bit latency ${lat}ms burst 32kbit
+sudo tc qdisc add dev eth0 parent 1:1 handle 10: tbf rate ${bw}bit burst 32kbit limit 10000
