edc8dea152ffb6f706e0b311cee927fcaa0b64c8
rocky1991 <hongchuan91@gmail.com>
2019-04-19 10:03:21 -0600

    minor changes

--- pa5/logs/q9/parse.py
+++
@@ -1,8 +0,0 @@
-with open('q9_results.log','w') as file:
-	file.write('')
-with open('q9_tshark.log','r') as file:
-	lines = file.readlines()
-	for line in lines:
-		if '54.188.89.98' in line:
-			with open('q9_results.log','a') as file:
-				file.write(line)
